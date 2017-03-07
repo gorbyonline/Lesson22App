5898f5e844bd7bbe9d9c637259785aac981be774
gorby <gorby@inbox.ru>
2017-03-07 15:37:16 +0300

    Ready for upload images

---
+++ Lesson22App/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Lesson22App
+//
+//  Created by Александр Горбачев on 07.03.17.
+//  Copyright © 2017 gorby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
